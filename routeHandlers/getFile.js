c8009242e89ce9b3b9612145fb6e09f74df6d5b9
Richard Artoul <richardartoul@gmail.com>
2015-07-22 23:16:46 -0700

    [Feature] Server will now check if file was stored with a password before delivering it to client, and if it was, will require the correct password

--- routeHandlers/getFile.js
+++ routeHandlers/getFile.js
@@ -3,6 +3,21 @@ var fs = require('fs');
 var isExpired = require('./utils/isExpired');
 var config = require('../config');
 var encryption = require('../encryption');
+var bcrypt = require('bcrypt');
+
+//helper function that reads file from disk and sends it to client
+var sendFile = function(req, res, file) {
+  var filePath = './upload/' + file._id + '/' + file.name;
+  fs.readFile(filePath, function (err,data) {
+    if (err) {
+      return res.status(404).send('File not found!');
+    }
+    //notifies browser of the proper filename and that it should be interpreted as a file download
+    res.setHeader('Content-disposition', 'attachment; filename=' + file.name);
+    //decrypt buffer before sending
+    res.status(200).send(encryption.decrypt(data));
+  });
+};
 
 module.exports = function(req, res) {
   //check if file is in database
@@ -10,20 +25,31 @@ module.exports = function(req, res) {
     if (foundFile && foundFile.length > 0) {
       //find returns an array so grab first index
       foundFile = foundFile[0];
+
       //Causes the links to expire after a certain period of time.
       if (isExpired(Date.now(), foundFile.date, config.linkExpireInHours)) {
         return res.status(400).send('Link expired');
       }
-      var filePath = './upload/' + foundFile._id + '/' + foundFile.name;
-      fs.readFile(filePath, function (err,data) {
-        if (err) {
-          return res.status(404).send('File not found!');
-        }
-        //notifies browser of the proper filename and that it should be interpreted as a file download
-        res.setHeader('Content-disposition', 'attachment; filename=' + foundFile.name);
-        //decrypt buffer before sending
-        res.status(200).send(encryption.decrypt(data));
-      });
+
+      //If file was submitted with a password, password is required to retrieve the file
+      if (foundFile.password && !req.body.password) {
+        return res.status(400).send('Password required to retrieve this file!');
+      }
+
+      //If file requires a password AND a password is provided, compare them to make sure that they match before delivering file.
+      if (foundFile.password && req.body.password) {
+        bcrypt.compare(req.body.password, foundFile.password, function(err, same) {
+          if (same) {
+            sendFile(req, res, foundFile);
+          }
+          else {
+            res.status(400).send('Wrong password!');
+          }
+        });
+      }
+      else {
+        sendFile(req, res, foundFile);
+      }
     }
     else {
       return res.status(404).send('File not found!');
