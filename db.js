9e61f60193b6fef7cf6f36fee46074e5621a37dd
Richard Artoul <richardartoul@gmail.com>
2015-07-23 18:21:25 -0700

    [Feature] Added a small line of code to allow deployment to heroku (MongoDB connection)

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 //Db connection
 var mongoose = require('mongoose');
 var config = require('./config');
-mongoose.connect(config.dbAddress);
+var serverAddress = MONGOLAB_URI || config.dbAddress;
+mongoose.connect(serverAddress);
 
 var FileSchema = new mongoose.Schema({
   //max password length is 20 characters
